d5fee56a74fc5e5b32957acb839d06e89fe754d7
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-10-31 14:51:07 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function superbowlWin(record){
+   const winning = record.find(isWin)
+   if (winning === undefined){;
+    return undefined;
+   } else return winning.year;
+    // (record.result === "W") ;
+}
+
+function isWin(indivRecord){
+    return indivRecord.result === "W"
+}
+
+
+
+
+// // for statement 
+
+// function superbowlWin(record){
+//     for (const element in record){
+//         if (element.result === "W"){
+//             return (element.year)
+//         }
+//     }
+// }
